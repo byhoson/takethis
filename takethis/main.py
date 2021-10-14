c95c3b3b45a09a263a2a91481421356673a9137a
Bjongho Son <bhson99@gmail.com>
2021-10-14 08:21:05 +0000

    [takethis] add main.py

---
+++ takethis/main.py
@@ -0,0 +1,10 @@
+import rank
+
+def main(career):
+    # career_sig : dict = rank.build_career_sig(career)
+    # courses : list = career_to_sig(career_sig)
+    # print(courses)
+    pass
+
+if __name__ == '__main__':
+    main()
