20273f79d9dacc8e792e7721ebf4024ba1b8292e
Bjongho Son <bhson99@gmail.com>
2021-10-14 08:38:03 +0000

    [scripts] add utf-8-sig encoding. resolves #1

--- scripts/parse-cse-courses.py
+++ scripts/parse-cse-courses.py
@@ -9,7 +9,7 @@ print("parsing cse-courses.csv...")
 
 results = []
 
-with open(DATA) as csv_file:
+with open(DATA, 'r', encoding='utf-8-sig') as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
   for row in csv_reader:
     result = {}
